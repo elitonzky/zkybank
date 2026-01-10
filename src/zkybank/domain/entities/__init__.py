89cc619296fc4c5c855694294c0f9624ea7822da
Eliton <eliton-jorge@hotmail.com>
2026-01-10 15:10:39 -0300

    feat: Add Account and LedgerEntry classes with factory methods

---
+++ src/zkybank/domain/entities/__init__.py
@@ -0,0 +1,4 @@
+from zkybank.domain.entities.account import Account
+from zkybank.domain.entities.ledger_entry import LedgerEntry, LedgerEntryType
+
+__all__ = ["Account", "LedgerEntry", "LedgerEntryType"]
